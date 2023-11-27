fe99f1640c4cb6623ebae3f995beb0d778edac68
Rogerio Homen <rhtechprog@outlook.com>
2023-11-27 08:05:39 -0300

    add mapa

--- dashboard_vendas.py
+++ dashboard_vendas.py
@@ -15,11 +15,27 @@ st.title('DASHBOARD DE VENDAS :shopping_trolley:')
 url = 'http://labdados.com/produtos'
 response = requests.get(url)
 dados = pd.DataFrame.from_dict(response.json())
+##tableas 
+receita_estados = dados.groupby('Local da compra')[['Preço']].sum()
+receita_estados = dados.drop_duplicates(subset='Local da compra')[['Local da compra', 'lat', 'lon']].merge(receita_estados, left_on='Local da compra', right_index=True).sort_values('Preço', ascending = False)
 
+##Graficos
+fig_mapas_receita = px.scatter_geo(receita_estados, 
+                                    lat= 'lat',
+                                    lon= 'lon',
+                                    scope= 'south america',
+                                    size= 'Preço',
+                                    template= 'seaborn',
+                                    hover_name= 'Local da compra',
+                                    hover_data= {'lat': False, 'lon': False},
+                                    title='Receita por Estados',)
+
+##Vizualização 
 coluna1, coluna2 = st.columns(2)
 
 with coluna1:
     st.metric('Receita', formatar_numero(dados['Preço'].sum(), 'R$'))
+    st.plotly_chart(fig_mapas_receita)
 with coluna2:
     st.metric('Quantidade de Vendas', formatar_numero(dados.shape[0]))
 
